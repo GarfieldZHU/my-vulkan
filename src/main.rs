b2ab7c222c13704d328e2e0404e50b1d88436cf3
jiazhu <garfield@bupt.edu.cn>
2020-09-09 21:02:08 +0800

    feat: add validation layer

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,27 @@ use vulkano::instance::{
     InstanceExtensions,
     ApplicationInfo,
     Version,
+    layers_list,
+    debug::{DebugCallback, MessageType, MessageSeverity},
 };
 
 
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
 
+const VALIDATION_LAYERS: &[&str] =  &[
+    "VK_LAYER_LUNARG_standard_validation"
+];
+
+#[cfg(all(debug_assertions))]
+const ENABLE_VALIDATION_LAYERS: bool = true;
+#[cfg(not(debug_assertions))]
+const ENABLE_VALIDATION_LAYERS: bool = false;
+
 #[allow(unused)]
 struct HelloTriangleApplication {
     instance: Arc<Instance>,
+    debug_callback: Option<DebugCallback>,
     events_loop: EventLoop<()>,
     window: Arc<Window>,
 }
@@ -31,9 +43,11 @@ impl HelloTriangleApplication {
     pub fn initialize() -> Self {
         let (events_loop, window) = Self::init_window();
         let instance = Self::create_instance();
+        let debug_callback = Self::setup_debug_callback(&instance);
 
         Self {
             instance,
+            debug_callback,
             events_loop,
             window,
         }
@@ -50,6 +64,10 @@ impl HelloTriangleApplication {
     }
 
     fn create_instance() -> Arc<Instance> {
+        if ENABLE_VALIDATION_LAYERS && !Self::check_validation_layer_support() {
+            println!("Validation layers requested, but not available!")
+        }
+
         let supported_extensions = InstanceExtensions::supported_by_core()
             .expect("failed to retrieve supported extensions");
         println!("Supported extensions: {:?}", supported_extensions);
@@ -61,9 +79,53 @@ impl HelloTriangleApplication {
             engine_version: Some(Version { major: 1, minor: 0, patch: 0 }),
         };
 
-        let required_extensions = vulkano_win::required_extensions();
-        Instance::new(Some(&app_info), &required_extensions, None)
-            .expect("failed to create Vulkan instance")
+        let required_extensions = Self::get_required_extensions();
+        if ENABLE_VALIDATION_LAYERS && Self::check_validation_layer_support() {
+            Instance::new(Some(&app_info), &required_extensions, VALIDATION_LAYERS.iter().cloned())
+                .expect("failed to create Vulkan instance")
+        } else {
+            Instance::new(Some(&app_info), &required_extensions, None)
+                .expect("failed to create Vulkan instance")
+        }
+    }
+
+    fn check_validation_layer_support() -> bool {
+        let layers: Vec<_> = layers_list().unwrap().map(|l| l.name().to_owned()).collect();
+        VALIDATION_LAYERS.iter()
+            .all(|layer_name| layers.contains(&layer_name.to_string()))
+    }
+
+    fn get_required_extensions() -> InstanceExtensions {
+        let mut extensions = vulkano_win::required_extensions();
+        if ENABLE_VALIDATION_LAYERS {
+            // TODO!: this should be ext_debug_utils (_report is deprecated), but that doesn't exist yet in vulkano
+            extensions.ext_debug_utils = true;
+        }
+
+        extensions
+    }
+
+    fn setup_debug_callback(instance: &Arc<Instance>) -> Option<DebugCallback> {
+        if !ENABLE_VALIDATION_LAYERS  {
+            return None;
+        }
+
+        let msg_severity = MessageSeverity {
+            error: true,
+            warning: true,
+            information: false,
+            verbose: true,
+        };
+
+        let msg_type =  MessageType {
+            general: true,
+            validation: true,
+            performance: true,
+        };
+
+        DebugCallback::new(&instance, msg_severity, msg_type, |msg| {
+            println!("validation layer: {:?}", msg.description);
+        }).ok()
     }
 
     fn main_loop(self) {
